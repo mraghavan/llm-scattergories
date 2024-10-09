eb57057979fc4d6f2fe69a5060d7e819ec3adf37
Manish Raghavan <mragh@mit.edu>
2024-10-09 17:10:02 -0400

    tentative generation and verification

--- generate_trees.py
+++ generate_trees.py
@@ -1,4 +1,3 @@
-import sys
 import time
 import os
 import re
@@ -11,7 +10,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--models', '-m', type=str, required=True)
 parser.add_argument('--verifier', '-v', type=str, default='llama3.1')
-parser.add_argument('--max_temperature', '-t', type=float, default=1.5)
+parser.add_argument('--max_temperatures', '-t', type=str, default='1.0')
 parser.add_argument('--num_instances', '-n', type=int, default=20)
 parser.add_argument('--use_hf', '-f', action='store_true', default=False)
 parser.add_argument('--output_dir', '-o', type=str, default='./trees')
@@ -37,9 +36,9 @@ def parse_pickle_filename(filename: str) -> tuple[str, str, float, str] | None:
         return None
     return m.group(1), m.group(2), float(m.group(3)), m.group(5)
 
-def get_v_filename(tree_dir: str, letter: str, category: str) -> str:
+def get_v_filename(tree_dir: str, letter: str, category: str, v_model_name: str) -> str:
     category = make_str_safe(category)
-    return f'{tree_dir}/{letter}_{category}_v.pkl'
+    return f'{tree_dir}/{letter}_{category}_{v_model_name}_v.pkl'
 
 def parse_v_filename(filename: str) -> tuple[str, str] | None:
     m = re.match(r'([A-Z])_(.+)_v.pkl', filename)
@@ -65,37 +64,14 @@ def create_tree_if_necessary(
         elapsed = time.time() - start
         print(f'Elapsed time: {elapsed:.2f} seconds')
         tree.standardize_tree()
-        tree.pickle_tree(fname)
+        tree.pickle_tree(fname, MODELS[engine.nickname])
     return tree
 
-def verify_tree(tree: CompletionNode, verifier: Verifier, category: str, letter: str):
-    if os.path.exists(get_v_filename(letter, category)):
+def verify_trees(trees: list[CompletionNode], verifier: Verifier, output_dir: str, category: str, letter: str):
+    v_filename = get_v_filename(output_dir, letter, category, verifier.engine.nickname)
+    if os.path.exists(v_filename):
         print('Verifier already exists')
-        with open(get_v_filename(letter, category), 'rb') as f:
-            verified_dict = pickle.load(f)
-            verified_y = verified_dict['yes']
-            verified_n = verified_dict['no']
-    else:
-        verified_y = set()
-        verified_n = set()
-    for node in tree.iter_leaves():
-        if node.text in verified_y or node.text in verified_n:
-            continue
-        if verifier.verify(node.text, category, letter):
-            print('Yes:', node.text)
-            verified_y.add(node.text)
-        else:
-            print('No:', node.text)
-            verified_n.add(node.text)
-    print('Verified yes:', verified_y)
-    print('Verified no:', verified_n)
-    verified_dict = {'yes': verified_y, 'no': verified_n}
-    return verified_dict
-
-def verify_trees(trees: list[CompletionNode], verifier: Verifier, category: str, letter: str):
-    if os.path.exists(get_v_filename(letter, category)):
-        print('Verifier already exists')
-        with open(get_v_filename(letter, category), 'rb') as f:
+        with open(v_filename, 'rb') as f:
             verified_dict = pickle.load(f)
             verified_y = verified_dict['yes']
             verified_n = verified_dict['no']
@@ -115,23 +91,29 @@ def verify_trees(trees: list[CompletionNode], verifier: Verifier, category: str,
     print('Verified yes:', verified_y)
     print('Verified no:', verified_n)
     verified_dict = {'yes': verified_y, 'no': verified_n}
+    with open(v_filename, 'wb') as f:
+        print(f'Writing to {v_filename}')
+        pickle.dump(verified_dict, f)
     return verified_dict
 
 if __name__ == '__main__':
-    # print(args)
-    # 1/0
+    print(args)
     models = args.models.split(',')
     for model in models:
         if model not in MODELS:
             raise ValueError(f'Invalid model: {model}')
+    max_temperatures = [float(t) for t in args.max_temperatures.split(',')]
+    if len(max_temperatures) == 1:
+        max_temperatures = [max_temperatures[0]] * len(models)
+    elif len(max_temperatures) != len(models):
+        raise ValueError('Number of models and number of max temperatures must match')
     random.seed(0)
     instances = get_random_instances(args.num_instances)
-    max_temperature = args.max_temperature
     print(instances)
     tree_map = {}
-    VERIFIER_MODEL_NAME = MODELS[args.verifier]
+    verifier_model_name = MODELS[args.verifier]
     start = time.time()
-    for nickname in models:
+    for nickname, max_temperature in zip(models, max_temperatures):
         print(f'Loading model: {nickname}')
         model_name = MODELS[nickname]
         engine = CE.get_completion_engine(model_name, max_temperature=max_temperature, nickname=nickname)
@@ -145,11 +127,10 @@ if __name__ == '__main__':
         del engine
     elapsed = time.time() - start
     print('Finished generating trees')
-    print(f'Total elapsed time: {elapsed:.2f} seconds')
-    sys.exit(0)
-    verifier = Verifier(*load(VERIFIER_MODEL_NAME))
+    print(f'Total elapsed time for tree generation: {elapsed:.2f} seconds')
+    verifier = Verifier(verifier_model_name, CE, nickname=args.verifier)
+    start = time.time()
     for (letter, category), trees in tree_map.items():
-        verified_dict = verify_trees(trees, verifier, category, letter)
-        with open(get_v_filename(letter, category), 'wb') as f:
-            print(f'Writing to {get_v_filename(letter, category)}')
-            pickle.dump(verified_dict, f)
+        verify_trees(trees, verifier, args.output_dir, category, letter)
+    elapsed = time.time() - start
+    print(f'Total elapsed time for verification: {elapsed:.2f} seconds')
