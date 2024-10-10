bce5ca0d1fe90c1cd1ac08761b3b51a2c60badeb
Manish Raghavan <mragh@mit.edu>
2024-10-10 19:56:22 -0400

    better split

--- analyze_games.py
+++ analyze_games.py
@@ -191,9 +191,7 @@ if __name__ == '__main__':
     print(len(all_jobs))
     job_num = args.job_num
     total_jobs = args.total_jobs
-    # contiguous jobs
-    jobs_per_task = len(all_jobs) // total_jobs + 1
-    my_jobs = all_jobs[jobs_per_task*job_num:jobs_per_task*(job_num+1)]
+    my_jobs = all_jobs[job_num::total_jobs]
     print(f'Job {job_num+1} of {total_jobs}: {my_jobs}')
     print(len(my_jobs))
     loaded_models = {}
