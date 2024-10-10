ab7ffaf67c7fa298e44bea21614ef61feeac3907
Manish Raghavan <mragh@mit.edu>
2024-10-10 15:20:38 -0400

    add nemotron, upgrade qwen

--- completion_hf.py
+++ completion_hf.py
@@ -9,10 +9,12 @@ MODELS = {
         'llama3.1': 'meta-llama/Llama-3.1-8B-Instruct',
         'llama3.2': 'meta-llama/Llama-3.2-1B-Instruct',
         'smollm': 'HuggingFaceTB/SmolLM-1.7B-Instruct',
-        'qwen2': 'Qwen/Qwen2-7B',
+        # 'qwen2': 'Qwen/Qwen2-7B', # (use 2.5 instead)
         # 'gemma2': 'google/gemma-2-2b-it', # (no system role in chat template)
         'mistral': 'mistralai/Mistral-7B-Instruct-v0.3',
         'phi3.5': 'microsoft/Phi-3.5-mini-instruct',
+        'nemotron': 'nvidia/Nemotron-Mini-4B-Instruct',
+        'qwen2.5': 'Qwen/Qwen2.5-7B-Instruct',
           }
 
 
