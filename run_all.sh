58213b96c55c7bbaa8b3cb94e2582baea5f6f144
Manish Raghavan <mragh@mit.edu>
2024-10-11 09:17:34 -0400

    run all

---
+++ run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gen_id=$(sbatch --parsable run_generate_trees_array.slurm)
+echo "Submitted generate_trees job with ID $gen_id"
+v_id=$(sbatch --parsable --dependency=afterok:$gen_id run_generate_trees.slurm)
+echo "Submitted verify_trees job with ID $v_id"
+sbatch --dependency=afterok:$v_id run_analyze_trees.slurm
