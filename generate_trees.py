5dde33f1be366ca0fac9463d1db3f8bfee552716
Manish Raghavan <mragh@mit.edu>
2024-10-18 13:46:01 -0400

    temps down

--- generate_trees.py
+++ generate_trees.py
@@ -21,15 +21,15 @@ parser.add_argument('--job_num', '-j', type=int, default=0)
 parser.add_argument('--num_jobs', '-t', type=int, default=1)
 
 MAX_TEMPS = {
-        'meta-llama/Meta-Llama-3-8B-Instruct': 1.8,
+        'meta-llama/Meta-Llama-3-8B-Instruct': 1.7,
         'meta-llama/Llama-3.1-8B-Instruct': 1.8,
-        'meta-llama/Llama-3.2-1B-Instruct': 1.5,
+        'meta-llama/Llama-3.2-1B-Instruct': 1.4,
         'HuggingFaceTB/SmolLM-1.7B-Instruct': 1.2,
         'Qwen/Qwen2-7B': 1.5,
         'google/gemma-2-2b-it': 1.5,
         'microsoft/Phi-3.5-mini-instruct': 2.2,
-        'mistralai/Mistral-7B-Instruct-v0.3': 1.8,
-        'nvidia/Nemotron-Mini-4B-Instruct': 1.8,
+        'mistralai/Mistral-7B-Instruct-v0.3': 1.6,
+        'nvidia/Nemotron-Mini-4B-Instruct': 1.7,
         # 'nvidia/Mistral-NeMo-Minitron-8B-Instruct': 1.9, # (too big for gpu)
         'Qwen/Qwen2.5-7B-Instruct': 2.0,
         'mlx-community/Meta-Llama-3.1-8B-Instruct-8bit': 1.5,
