76a1cd276ec78d33154f4a81a76a01eb1ba9976f
Manish Raghavan <mragh@mit.edu>
2024-10-24 15:33:26 -0700

    run all for samples

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-gen_id=$(sbatch --parsable run_generate_trees_array.slurm)
+gen_id=$(sbatch --parsable run_generate_samples.slurm)
 echo "Submitted generate_trees job with ID $gen_id"
-v_id=$(sbatch --parsable --dependency=afterok:$gen_id run_generate_trees.slurm)
+v_id=$(sbatch --parsable --dependency=afterok:$gen_id run_verify_samples.slurm)
 echo "Submitted verify_trees job with ID $v_id"
-sbatch --dependency=afterok:$v_id run_analyze_games.slurm
