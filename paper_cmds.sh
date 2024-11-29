e017749f01eac71fb2e2998556f27c68861c66a2
Manish Raghavan <mragh@mit.edu>
2024-11-29 16:31:38 -0500

    paper cmds

--- paper_cmds.sh
+++ paper_cmds.sh
@@ -2,23 +2,24 @@
 
 NUM_INSTANCES=25
 NUM_SAMPLES=2000
+MODELS=gemma2,llama3.1,llama3.2,mistral,nemotron,phi3.5
 
 
 # Should run the following on a cluster, and might want to split the them over array jobs
 # Generate samples (can reduce -s 2000 to something like 100 for faster testing)
-python3 generate_samples.py -m nemotron,phi3.5,llama3.2,mistral,smollm,llama3.1 -n $NUM_INSTANCES -s $NUM_SAMPLES -b 6
+python3 generate_samples.py -m $MODELS -n $NUM_INSTANCES -s $NUM_SAMPLES -b 6
 
 # Verify samples
 # Current slurm script isn't set up to run as an array job. Can modify to do so, but would also need to modify verify_samples.py
-python3 verify_samples.py -m nemotron,phi3.5,llama3.2,mistral,smollm,llama3.1 -n $NUM_INSTANCES -v qwen2.5
+python3 verify_samples.py -m $MODELS -n $NUM_INSTANCES -v qwen2.5
 
 # Could use ./run_all.sh instead of the above two commands if running on a cluster.
 # Need to appropriately modify run_generate_samples.slurm and run_verify_samples.slurm
 
 # Analyze equilibria (can be done locally)
-python3 analyze_samples.py -m nemotron,phi3.5,llama3.2,mistral,smollm,llama3.1 -v qwen2.5
+python3 analyze_samples.py -m $MODELS -v qwen2.5
 
 # Probably should run this on a cluster (see run_analyze_pairwise.slurm)
-python3 analyze_pairwise.py -m nemotron,phi3.5,llama3.2,mistral,smollm,llama3.1 -v qwen2.5
+python3 analyze_pairwise.py -m $MODELS -v qwen2.5
 
-./plot_cmds.sh
+./plot_cmds.sh $MODELS
--- plot_cmds.sh
+++ plot_cmds.sh
@@ -1,17 +1,20 @@
+MODELS=$1
 # plots
 # Main temp and utility plots (creates 5 figures)
-python3 make_plots.py -m gemma2,mistral,nemotron,llama3.2,phi3.5,llama3.1
+python3 make_plots.py -m $MODELS
 
 # Utility as a function of temp for different values of n
 # Creates 6 figures
-python3 plot_opt_over_temp.py -m phi3.5,llama3.1,llama3.2,gemma2,mistral,nemotron -v qwen2.5
+python3 plot_opt_over_temp.py -m $MODELS -v qwen2.5
 
 # Inversions: Weighted inversions and prob mass captured (2 figures)
-python3 plot_inversions.py -m phi3.5,llama3.1,llama3.2,gemma2,mistral,nemotron
+python3 plot_inversions.py -m $MODELS
+# Inversions on d
+python3 plot_prs.py -m $MODELS -v qwen2.5
 
 # Pairwise
 # Separately run 2 for main figures
 python3 plot_pairwise.py -m llama3.1,llama3.2
 python3 plot_pairwise.py -m llama3.1,phi3.5
 # One big plot with all pairwise comparisons
-python3 make_big_pairwise_plot.py -m llama3.1,llama3.2,phi3.5,gemma2,nemotron,mistral
+python3 make_big_pairwise_plot.py -m $MODELS
