2376cfcf1f405e4581bd4bd244e092f181bde5dc
Manish Raghavan <mragh@mit.edu>
2025-05-20 10:37:11 -0400

    fix

--- evaluate_answer_likelihoods.py
+++ evaluate_answer_likelihoods.py
@@ -209,8 +209,6 @@ def main():
     
     # Process each job
     for letter, category, model_config in my_jobs:
-        if model_config['model'] != 'llama3.2':
-            continue
         process_job(letter, category, model_config, fm, args.min_count)
 
 if __name__ == "__main__":
